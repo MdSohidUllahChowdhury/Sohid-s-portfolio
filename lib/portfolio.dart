39eaf2ec12f91455fbf32c3eb3bc168c8a688072
Md.Sohid <mdsohidullahchowdhury1@gmail.com>
2024-01-27 21:31:36 +0600

    Update Skills

--- lib/portfolio.dart
+++ lib/portfolio.dart
@@ -45,41 +45,145 @@ class _PortfolioState extends State<Portfolio> {
           child: ElevatedButton(
               onPressed: () {
                 showModalBottomSheet(
-                    backgroundColor: const Color.fromARGB(255, 235, 244, 236),
+                    backgroundColor: Color.fromARGB(255, 83, 90, 92),
                     enableDrag: true,
                     context: context,
                     builder: (context) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.max,
-
-            
-
-                        children: [
-                          SizedBox(height: 35,),
-                          Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          
-                           
-                            children: [
-                            
-                            
-                           
-                              
-                              Card(
-                                child: Icon(FontAwesomeIcons.html5, size: 50),
-                              ),
-                               Card(
-                                child: Icon(FontAwesomeIcons.css3, size: 50),
-                              ),
-                               Card(
-                                child: Icon(FontAwesomeIcons.bootstrap, size: 50),
-                              ),
-                               Card(
-                                child: Icon(FontAwesomeIcons.github, size: 50),
-                              ),
-
-                            ],
-                          ),
-                        ],
+                      return SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            SizedBox(
+                              height: 35,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "My Skills Are :",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Card(
+                                  child: Icon(FontAwesomeIcons.html5, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.css3, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.bootstrap,
+                                      size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.github, size: 45),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Card(
+                                  child: Icon(FontAwesomeIcons.java, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.android, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.python, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.wordpress,
+                                      size: 45),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.angular, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.react, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.c, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.amazon, size: 45),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.facebook, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.rust, size: 45),
+                                ),
+                                Card(
+                                  child: Icon(FontAwesomeIcons.linux, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.windows, size: 45),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.appStore, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.bitcoin, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.twitter, size: 45),
+                                ),
+                                Card(
+                                  child:
+                                      Icon(FontAwesomeIcons.linkedin, size: 45),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       );
                     });
               },
