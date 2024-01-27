c12d1f2ef1cd37f541f6d11064889d87e762e986
Md.Sohid <mdsohidullahchowdhury1@gmail.com>
2024-01-27 17:34:42 +0600

    Update

--- lib/portfolio.dart
+++ lib/portfolio.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Portfolio extends StatefulWidget {
   const Portfolio({super.key});
@@ -13,9 +14,9 @@ class _PortfolioState extends State<Portfolio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // backgroundColor: Color.fromARGB(168, 0, 0, 0),
+      // backgroundColor: Color.fromARGB(168, 0, 0, 0),
 
-        appBar: AppBar(
+      appBar: AppBar(
           leading: PopupMenuButton(
               icon: Icon(Icons.menu),
               color: Colors.black,
@@ -29,7 +30,6 @@ class _PortfolioState extends State<Portfolio> {
                         onPressed: () {},
                       ),
                     ),
-                    
                     PopupMenuItem(
                       child: TextButton(
                         child: Text(
@@ -41,37 +41,49 @@ class _PortfolioState extends State<Portfolio> {
                     )
                   ])),
 
-body: Center(child: ElevatedButton(onPressed: (){
-  showModalBottomSheet(
-    backgroundColor: Colors.green,
-    enableDrag: true,
-  
-   context: context, builder: (context){
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        ListTile(
-                    title: Text('GitHub'),
-                    subtitle: Text('It save your project and can access able from anywere'),
-        ),
-         ListTile(
-                    title: Text('GitHub'),
-                    subtitle: Text('It save your project and can access able from anywere'),
-        ),
-         ListTile(
-                    title: Text('GitHub'),
-                    subtitle: Text('It save your project and can ac3cess able from anywere'),
-        ),
-         ListTile(
-                    title: Text('GitHub'),
-                    subtitle: Text('It save your project and can access able from anywere'),
-        ),
-      ],
-    );
-  } 
-  );
-}, child: Text("Press On It"))),
-                  
+      body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    backgroundColor: const Color.fromARGB(255, 235, 244, 236),
+                    enableDrag: true,
+                    context: context,
+                    builder: (context) {
+                      return Column(
+                        mainAxisSize: MainAxisSize.max,
+
+            
+
+                        children: [
+                          SizedBox(height: 35,),
+                          Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          
+                           
+                            children: [
+                            
+                            
+                           
+                              
+                              Card(
+                                child: Icon(FontAwesomeIcons.html5, size: 50),
+                              ),
+                               Card(
+                                child: Icon(FontAwesomeIcons.css3, size: 50),
+                              ),
+                               Card(
+                                child: Icon(FontAwesomeIcons.bootstrap, size: 50),
+                              ),
+                               Card(
+                                child: Icon(FontAwesomeIcons.github, size: 50),
+                              ),
+
+                            ],
+                          ),
+                        ],
+                      );
+                    });
+              },
+              child: Text("Press On It"))),
     );
   }
 }
